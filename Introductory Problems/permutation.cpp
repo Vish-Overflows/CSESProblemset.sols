bbdf042031234ac3b8912ed0088eb11479702b1c
Vish-Overflows <24110389@iitgn.ac.in>
2025-06-16 15:01:34 +0530

    Create permutation.cpp

---
+++ Introductory Problems/permutation.cpp
@@ -0,0 +1,21 @@
+//A permutation of integers 1,2,\ldots,n is called beautiful if there are no adjacent elements whose difference is 1.
+// Given n, construct a beautiful permutation if such a permutation exists
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int n;
+    cin >> n;
+    if (n <= 3) 
+    {
+        cout << "NO SOLUTION\n";
+        return 0;
+    }
+    for (int i = 2; i <= n; i += 2) 
+        cout << i << " ";
+    for (int i = 1; i <= n; i += 2) 
+        cout << i << " ";
+    cout << "\n";
+    return 0;
+}
+
